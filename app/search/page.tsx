f9ac713af4fc8d2912b19dcbd61d059795562587
YiHsuan <v61265@gmail.com>
2025-04-24 13:56:01 +0800

    refactor(search): add feature toggle of search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,9 @@
+import { ENV } from '@/constants/config'
 import SearchComponent from './_components/search-component'
+import { notFound } from 'next/navigation'
 
 export default function GcseSearch() {
+  if (ENV === 'staging' || ENV === 'prod') notFound()
   return (
     <div>
       <SearchComponent />
